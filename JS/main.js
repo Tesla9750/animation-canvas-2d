fb33ab150920676305fa2bb3129a90cb2b7ff59c
tesla <l21200582@pachuca.tecnm.mx>
2024-04-24 09:56:51 -0600

    |V3|Correcion de aparicion|

--- JS/main.js
+++ JS/main.js
@@ -61,10 +61,10 @@ let NumeroCirculos = 10;
 
 for (let i = 0; i < NumeroCirculos; i++) {
 
-    let randomX = Math.random() * window_width;
-    let randomY = Math.random() * window_height;
-    let randomRadius = Math.floor(Math.random() * 100 + 25);
-    let randomSpeed = Math.floor(Math.random() * 10) + 1;
+    let randomX = Math.random() * (window_width-200)+100;
+    let randomY = Math.random() * (window_height-200)+100;
+    let randomRadius = Math.floor(Math.random() * 90 + 25);
+    let randomSpeed = Math.floor(Math.random() * 8) + 1;
 
     let miCirculo = new Circle(randomX, randomY, randomRadius, 'Blue', i + 1, randomSpeed);
 
